70a3dfd58c3ccc4ba293eea5d675ebae8346eb29
Gustavo Jorge Geres <gjgeres@outlook.com>
2024-10-24 11:35:58 -0300

    Update

--- PRJ_Sist_Votacao/src/Sist_Vot_Main.java
+++ PRJ_Sist_Votacao/src/Sist_Vot_Main.java
@@ -10,8 +10,19 @@ import javax.swing.JOptionPane;
  * 
  */
 public class Sist_Vot_Main {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException{
+        Votacao[] vot = new Votacao[5];
+        Votacao2024 vot24 = new Votacao2024();
+        int opc = 0;
+        while(opc != 9){
+            opc = Integer.parseInt(JOptionPane.showInputDialog("*** SISTEMA DE VOTAÇÃO ***\n1 - Carregar Seção/Número Eleitor \n2 - Classificar por Seção "
+                    + "\n3 - Gravar Registros \n4 - Mostrar Indicadores \n9 - Finalizar"));
+            switch(opc){
+                case 1: 
+                    vot = vot24.CadastroVotacao(vot);
+                    break;
+            }
+        }
     }
  
 }
--- PRJ_Sist_Votacao/src/Votacao2024.java
+++ PRJ_Sist_Votacao/src/Votacao2024.java
@@ -5,14 +5,42 @@ public class Votacao2024 {
         int x;
         String fileName = "ArquivoVotos.txt";
         BufferedWriter save = new BufferedWriter(new FileWriter(fileName));
-        for(x = 0; x < 4; x++){
+        for(x = 0; x < 5; x++){
             vot[x] = new Votacao();
         }
-        for(x = 0; x < 4; x++){
+        x = 0;
+        while(x < 5){
             vot[x].NumeroSecao = Integer.parseInt(JOptionPane.showInputDialog("Digite o número da seção"));
-                save.write(Integer.toString(vot[x].));
+            vot[x].NumeroCandidato = Integer.parseInt(JOptionPane.showInputDialog("Digite o número do Candidato"));
+            
+            if((vot[x].NumeroSecao <= 10) && (vot[x].NumeroSecao > 0)){
+                save.write(Integer.toString(vot[x].NumeroSecao));
+                save.newLine();
+                x++;
+            } else{
+                JOptionPane.showMessageDialog(null, "Dados Errados! Digite um número de seção válido");
+                if(x > 0){
+                    x = x - 1;
+                } else{
+                    x = 0;
+                }
+            }
+            if((vot[x].NumeroCandidato <= 300) && (vot[x].NumeroCandidato >= 1)){
+                save.write(Integer.toString(vot[x].NumeroCandidato));
+                save.newLine();
+                x++;
+            } else{
+                JOptionPane.showMessageDialog(null, "Dados Errados! Digite um número de candidato válido");
+                if(x > 0){
+                    x = x - 1;
+                } else{
+                    x = 0;
+                }
+            }
+            
         }
-        
-        
+        System.out.println("GRAVACAO FEITA COM SUCESSO ");
+        save.close();
+        return vot;   
     }
 }
