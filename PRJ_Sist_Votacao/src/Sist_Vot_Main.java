6543c3f96bf2b1ca79683f8933dbe383521472e7
Gustavo Jorge Geres <gjgeres@outlook.com>
2024-10-25 11:24:47 -0300

    Update

--- PRJ_Sist_Votacao/src/Sist_Vot_Main.java
+++ PRJ_Sist_Votacao/src/Sist_Vot_Main.java
@@ -11,17 +11,41 @@ import javax.swing.JOptionPane;
  */
 public class Sist_Vot_Main {
     public static void main(String[] args) throws IOException{
-        Votacao[] vot = new Votacao[5];
+        Votacao[] vot = new Votacao[200];
         Votacao2024 vot24 = new Votacao2024();
         int opc = 0;
         while(opc != 9){
-            opc = Integer.parseInt(JOptionPane.showInputDialog("*** SISTEMA DE VOTAÇÃO ***\n1 - Carregar Seção/Número Eleitor \n2 - Classificar por Seção "
+            opc = Integer.parseInt(JOptionPane.showInputDialog("*** SISTEMA DE VOTAÇÃO ***"
+                    + "\n1 - Carregar Seção/Número Eleitor \n2 - Classificar por Seção "
                     + "\n3 - Gravar Registros \n4 - Mostrar Indicadores \n9 - Finalizar"));
             switch(opc){
                 case 1: 
                     vot = vot24.CadastroVotacao(vot);
                     break;
-            }
+                case 2: 
+                    vot = vot24.ClassificaVot(vot);
+                    break;
+                case 3:
+                    vot = vot24.GravaVot(vot);
+                    break;
+                case 4:
+                    int opc2 = 0;
+                        
+                    while(opc2 != 9){
+                        opc2 = Integer.parseInt(JOptionPane.showInputDialog("*** MOSTRAR INDICADORES ***"
+                            + "\n1 - Eleitores por Seção \n2 - Seção com Maior e Menor Número de Eleitores "
+                            + "\n3 - Quantidade de votos por candidato \n4 - 10 primeiros colocados "
+                                + "\n9 - Finalizar"));
+                        switch(opc2){
+                            case 1:
+                                vot24.PEleitoresSecao(vot);
+                                break;
+                        }
+                    }
+                    break;
+                    
+            }   
+            
         }
     }
  
--- PRJ_Sist_Votacao/src/Votacao2024.java
+++ PRJ_Sist_Votacao/src/Votacao2024.java
@@ -1,46 +1,98 @@
 import java.io.*;
 import javax.swing.*;
+import java.util.Random;
 public class Votacao2024 {
     public Votacao [ ] CadastroVotacao(Votacao[] vot) throws IOException{
         int x;
-        String fileName = "ArquivoVotos.txt";
-        BufferedWriter save = new BufferedWriter(new FileWriter(fileName));
-        for(x = 0; x < 5; x++){
+        //String fileName = "ArquivoVotos.txt";
+        //BufferedWriter save = new BufferedWriter(new FileWriter(fileName));
+        for(x = 0; x < 200; x++){
             vot[x] = new Votacao();
         }
+        // Gerador de números
+        Random y = new Random();
+        
         x = 0;
-        while(x < 5){
-            vot[x].NumeroSecao = Integer.parseInt(JOptionPane.showInputDialog("Digite o número da seção"));
-            vot[x].NumeroCandidato = Integer.parseInt(JOptionPane.showInputDialog("Digite o número do Candidato"));
+        while(x < 200){
+            vot[x].NumeroSecao = y.nextInt(11);
+            vot[x].NumeroCandidato = y.nextInt(301);
+            //vot[x].NumeroSecao = Integer.parseInt(JOptionPane.showInputDialog("Digite o número da seção"));
+            //vot[x].NumeroCandidato = Integer.parseInt(JOptionPane.showInputDialog("Digite o número do Candidato"));
             
-            if((vot[x].NumeroSecao <= 10) && (vot[x].NumeroSecao > 0)){
-                save.write(Integer.toString(vot[x].NumeroSecao));
-                save.newLine();
+            if(((vot[x].NumeroSecao <= 10) && (vot[x].NumeroSecao > 0)) && ((vot[x].NumeroCandidato <= 300)
+                    && (vot[x].NumeroCandidato >= 1))){
+                //save.write(Integer.toString(vot[x].NumeroSecao));
+                //save.newLine();
+                //save.write(Integer.toString(vot[x].NumeroCandidato));
+                //save.newLine();
                 x++;
-            } else{
-                JOptionPane.showMessageDialog(null, "Dados Errados! Digite um número de seção válido");
-                if(x > 0){
-                    x = x - 1;
-                } else{
-                    x = 0;
-                }
-            }
-            if((vot[x].NumeroCandidato <= 300) && (vot[x].NumeroCandidato >= 1)){
-                save.write(Integer.toString(vot[x].NumeroCandidato));
-                save.newLine();
-                x++;
-            } else{
-                JOptionPane.showMessageDialog(null, "Dados Errados! Digite um número de candidato válido");
-                if(x > 0){
-                    x = x - 1;
-                } else{
-                    x = 0;
-                }
-            }
-            
+            } /*else{
+                JOptionPane.showMessageDialog(null, "Dados Errados! Digite um número de seção ou de candidato válido");
+            }*/   
         }
         System.out.println("GRAVACAO FEITA COM SUCESSO ");
-        save.close();
+        //save.close();
         return vot;   
     }
+    
+    public Votacao [] ClassificaVot(Votacao []vot) throws IOException{
+        int x, y;
+        //String fileName = "ArquivoVotos.txt";
+        //BufferedReader Read1 = new BufferedReader(new FileReader(fileName));
+        Votacao[] aux = new Votacao[1];
+        for(x = 0; x < 1; x++){
+            aux[x] = new Votacao();
+        }
+        /*for(x = 0; x < 200;x++){
+            vot[x].NumeroSecao = Integer.parseInt(Read1.readLine());
+            vot[x].NumeroCandidato = Integer.parseInt(Read1.readLine());
+        }*/
+        for(x = 0; x < 199; x++){
+            for(y = (x + 1); y < 200; y++){
+                if(vot[x].NumeroSecao > vot[y].NumeroSecao){
+                    aux[0] = vot[x];
+                    vot[x] = vot[y];
+                    vot[y] = aux[0];
+                }
+            }
+        }
+        return vot;
+    }
+    
+    public Votacao[] GravaVot(Votacao [] vot) throws IOException{
+        String fileName = "ArquivoVotos.txt";
+        BufferedWriter save = new BufferedWriter(new FileWriter(fileName));
+        int x;
+        for(x = 0; x < 200; x++){
+            save.write(Integer.toString(vot[x].NumeroSecao));
+            save.newLine();
+            save.write(Integer.toString(vot[x].NumeroCandidato));
+            save.newLine();
+        }
+        save.close();
+        return vot;
+    }
+    
+    public void PEleitoresSecao(Votacao [] vot) throws IOException{
+        int i,j;
+        String fileName = "ArquivoVotos.txt";
+        BufferedReader read = new BufferedReader(new FileReader(fileName));
+        for(i = 0; i < 200; i++){
+            vot[i].NumeroSecao = Integer.parseInt(read.readLine());
+            vot[i].NumeroCandidato = Integer.parseInt(read.readLine());
+        }
+        int somaSecao[] = new int[200];
+        for(i = 0; i < 199; i++){
+            for(j = (i+1); j < 200; j++){
+                if(vot[i].NumeroSecao == vot[j].NumeroSecao){
+                    somaSecao[i] = somaSecao[i] + 1;
+                }    
+            }  
+        }
+        for(i = 0; i < 200; i++){
+            System.out.println("Na Secao "+(i+1)+" houveram "+somaSecao[i]+" eleitores.");
+        }
+    }
+    
+    
 }
